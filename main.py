e0321f9321e92a27a558d5a9125301d0f72557e6
Julway <anna_gorny@hotmail.com>
2019-10-31 15:28:19 +0100

    new Session token

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from model import User, db
 import hashlib
+import uuid
 
 app = Flask(__name__)
 db.create_all()   #erstellt neue Datenbank und Tabelle
@@ -8,9 +9,9 @@ db.create_all()   #erstellt neue Datenbank und Tabelle
 
 @app.route("/")
 def index():
-    email_adress = request.cookies.get("email")
-    if email_adress:
-        user = db.query(User).filter_by(email=email_adress).first()
+    session_token = request.cookies.get("session_token")
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
     else:
         user = None
     return render_template('index.html', user=user)
@@ -21,17 +22,25 @@ def login():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
     password = request.form.get("user-password")
-    password = hashlib.sha256(password.encode()).hexdigest()
+    hashed_pw = hashlib.sha256(password.encode()).hexdigest()
+
     #neues Objekt User(Model
-    user = db.query(User).filter_by(email=email).first
+    user = db.query(User).filter_by(email=email).first()
     if not user:
-        user = User(name=name, email=email, password=password)
+        user = User(name=name, email=email, password=hashed_pw)
         db.add(user)
         db.commit()
-    #cookie
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie("email", email)
-    return response
+    if hashed_pw != user.password:
+        return "Wrong Password! Tra again!"
+    elif hashed_pw == user.password:
+        session_token = str(uuid.uuid4())
+        user.session_token = session_token
+        db.add(user)
+        db.commit()
+        #cookie
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("session_token", session_token, httponly=True, samesite="Strict")
+        return response
 
 
 if __name__ == '__main__':
