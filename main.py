048cb16639a97c92e9b3cb2cf562253968864466
Julway <anna_gorny@hotmail.com>
2019-10-31 14:42:32 +0100

    new hash password

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from model import User, db
+import hashlib
 
 app = Flask(__name__)
 db.create_all()   #erstellt neue Datenbank und Tabelle
@@ -20,6 +21,7 @@ def login():
     name = request.form.get("user-name")
     email = request.form.get("user-email")
     password = request.form.get("user-password")
+    password = hashlib.sha256(password.encode()).hexdigest()
     #neues Objekt User(Model
     user = db.query(User).filter_by(email=email).first
     if not user:
